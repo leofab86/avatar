ce803066c8b158f8571001f382afb4855c036218
markfields1982 <markfields1982@gmail.com>
2020-04-05 14:45:31 -0400

    finally it works!

--- portfolio/management/commands/runprod.py
+++ portfolio/management/commands/runprod.py
@@ -5,8 +5,8 @@ from portfolio.management.commandclasses import SubprocessCommand
 class Command(SubprocessCommand):
     help = 'Builds Portfolio distribution files'
     commands = [
-        'cd portfolio/reactserver ; npm run build:server',
-        'cd portfolio/reactserver ; npm run build:client',
-        'cd portfolio/reactserver ; ' + settings.REACTSERVER_ENV('production') + ' npm run watch:server',
-        'python3 manage.py runserver',
+        'cd portfolio/reactserver ;' +
+        'npm run build:server ; ' +
+        'npm run build:client ; ' +
+        settings.REACTSERVER_ENV('production') + ' npm run start:server',
     ]
