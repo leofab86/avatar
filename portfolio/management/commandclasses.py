7f9c03fd285d11fa1c47032075bb0bca4f7e7241
Leo <leofab86@gmail.com>
2020-04-08 07:27:00 -0400

    fixed commands

--- portfolio/management/commandclasses.py
+++ portfolio/management/commandclasses.py
@@ -1,28 +1,32 @@
 from django.core.management.base import BaseCommand
 
 from subprocess import Popen
-from sys import stdout, stdin, stderr
-import time, os, signal
+from sys import stdout, stdin, stderr, exit
 
 
 class SubprocessCommand(BaseCommand):
+    commands = []
 
     def handle(self, *args, **keywargs):
         proc_list = []
 
         for command in self.commands:
-            print('Running: ' + command)
-            proc_list.append({
+            print('RUNNING: ' + command)
+            proc = {
                 'command': command,
-                'process': Popen(command, shell=True, stdin=stdin, stdout=stdout, stderr=stderr)
-            })
+                'process': Popen(command, shell=True, stdin=stdin, stdout=stdout, stderr=stderr),
+            }
+            proc_list.append(proc)
 
         try:
-            while True:
-                time.sleep(10)
+            for proc in proc_list:
+                proc['process'].wait()
+                print('COMPLETE: ' + proc['command'])
+            exit()
+
         except KeyboardInterrupt:
             print(' ')
-            print('KILLING PROCESSES STARTED BY THESE COMMANDS: ')
+            print('KILLING: ')
             for proc in proc_list:
                 print('  ' + proc['command'])
-                os.kill(proc['process'].pid, signal.SIGKILL)
+                exit()
