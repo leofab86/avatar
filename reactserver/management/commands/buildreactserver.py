71a61437c9c559eb4f2ecf6fe0a4e807e8774556
Leo <leofab86@gmail.com>
2020-07-04 09:25:26 -0400

    pushing buildreactserver bundles to s3 bucket

--- reactserver/management/commands/buildreactserver.py
+++ reactserver/management/commands/buildreactserver.py
@@ -4,7 +4,9 @@ from reactserver.management.commandclasses import SubprocessCommand
 install_and_build = f'cd {settings.REACTSERVER_PATH} ; ' + \
     'npm install ; ' + \
     'npm run build:server ; ' + \
-    'npm run build:client ;'
+    'npm run build:client ; ' + \
+    'aws s3 cp /opt/bitnami/apps/django/django_projects/avatar/static/bundle.css s3://avatar-static/ --acl public-read ; ' + \
+    'aws s3 cp /opt/bitnami/apps/django/django_projects/avatar/static/client.bundle.js s3://avatar-static/ --acl public-read;'
 
 
 class Command(SubprocessCommand):
